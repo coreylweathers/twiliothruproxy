618bf8898d8b939079014f754f47d6c55688b91e
Corey Weathers <clw895@msn.com>
2018-01-05 14:48:16 -0800

    Adds a working phone call project to source control

---
+++ Helper.cs
@@ -0,0 +1,9 @@
+namespace Proxy
+{
+    public static class Helper
+    {
+        public static readonly string ACCOUNT_SID = "REPLACE_ACCOUNT_SID_HERE";
+        public static readonly string AUTH_TOKEN = "REPLACE_AUTH_TOKEN_HERE";
+        public static readonly string TWILIO_NUMBER = "REPLACE_PHONE_NUMBER_HERE";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toPhoneNumber = "REPLACE_TO_PHONE_NUMBER_HERE";
+            TwilioClient.Init(Helper.ACCOUNT_SID,Helper.AUTH_TOKEN);
+
+            try
+            {
+                var callResult = CallResource.Create(
+                    to: new PhoneNumber(toPhoneNumber),
+                    from: new PhoneNumber(Helper.TWILIO_NUMBER),
+                    url: new Uri("http://demo.twilio.com/docs/voice.xml")
+                );
+                Console.WriteLine(callResult.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred: {ex.Message}");
+            }
+        }
+    }
+}
