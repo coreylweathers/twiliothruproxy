2b0bbac0213de1a9e10e8301069074fbc5038c8d
Corey Weathers <clw895@msn.com>
2018-01-09 12:04:03 -0800

    Cleans up the using statements and changes the to phone number to use the Helper file

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
@@ -13,13 +9,11 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
-            var toPhoneNumber = "REPLACE_TO_PHONE_NUMBER_HERE";
             TwilioClient.Init(Helper.ACCOUNT_SID,Helper.AUTH_TOKEN);
-
             try
             {
                 var callResult = CallResource.Create(
-                    to: new PhoneNumber(toPhoneNumber),
+                    to: new PhoneNumber(Helper.TO_PHONE_NUMBER),
                     from: new PhoneNumber(Helper.TWILIO_NUMBER),
                     url: new Uri("http://demo.twilio.com/docs/voice.xml")
                 );
