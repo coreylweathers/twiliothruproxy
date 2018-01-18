5b00a593ff446af0b39dc757c6b4f52d14f8702f
Corey Weathers <clw895@msn.com>
2018-01-18 10:37:40 -0800

    Changes the name of the TO_PHONE_NUMBER value

--- Helper.cs
+++ Helper.cs
@@ -5,7 +5,7 @@ namespace Proxy
         public static readonly string ACCOUNT_SID = "REPLACE_ACCOUNT_SID_HERE";
         public static readonly string AUTH_TOKEN = "REPLACE_AUTH_TOKEN_HERE";
         public static readonly string TWILIO_NUMBER = "REPLACE_PHONE_NUMBER_HERE";
-        public static readonly string TO_PHONE_NUMBER = "REPLACE_DESTINATION_NUMBER_HERE";
+        public static readonly string TO_PHONE_NUMBER = "REPLACE_TO_NUMBER_HERE";
 
     }
 }
