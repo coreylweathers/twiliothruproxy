e56fb9abffa40a7f90e051d4803c1c77b46be788
Corey Weathers <clw895@msn.com>
2018-01-18 13:38:42 -0800

    Updates the value to use the YOUR_ nomenclature

--- Helper.cs
+++ Helper.cs
@@ -2,9 +2,9 @@ namespace Proxy
 {
     public static class Helper
     {
-        public static readonly string ACCOUNT_SID = "REPLACE_ACCOUNT_SID_HERE";
-        public static readonly string AUTH_TOKEN = "REPLACE_AUTH_TOKEN_HERE";
-        public static readonly string TWILIO_NUMBER = "REPLACE_PHONE_NUMBER_HERE";
-        public static readonly string TO_PHONE_NUMBER = "REPLACE_TO_NUMBER_HERE";
+        public static readonly string ACCOUNT_SID = "YOUR_ACCOUNT_SID_HERE";
+        public static readonly string AUTH_TOKEN = "YOUR_AUTH_TOKEN_HERE";
+        public static readonly string TWILIO_NUMBER = "YOUR_TWILIO_NUMBER_HERE";
+        public static readonly string DESTINATION_NUMBER = "YOUR_DESTINATION_NUMBER_HERE";
     }
 }
